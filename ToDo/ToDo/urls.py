16a5a82e19600801b4ae184d2d4dac0d8b41c7b1
vano <vanuartw@mail.ru>
2024-04-23 11:50:04 +0300

    todo

--- ToDo/ToDo/urls.py
+++ ToDo/ToDo/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from .swagger import urlpatterns as swag_url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
 
 ]
 urlpatterns += swag_url
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ToDo/api/models.py
+++ ToDo/api/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
 
 # Create your models here.
 
+class CustomUser(AbstractUser):
+    photo = models.ImageField(upload_to=f'{settings.BASE_DIR}/static', blank=True)
 
+    def __str__(self):
+        return f'{self.id}_{self.username}'

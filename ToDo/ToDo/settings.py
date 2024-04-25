c598750bb6cde1421d29ac92807324753d01a2d9
vano <vanuartw@mail.ru>
2024-04-25 12:14:40 +0300

    task create

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -127,6 +127,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+DJOSER = {
+    'LOGIN_FIELD':'email'
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/

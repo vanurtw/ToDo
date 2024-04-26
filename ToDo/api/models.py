60cc0e16fed75b4613bb2ac5a0634c5f07b3fd1d
vano <vanuartw@mail.ru>
2024-04-26 12:31:26 +0300

    admin

--- ToDo/api/models.py
+++ ToDo/api/models.py
@@ -13,7 +13,7 @@ def user_directory_path(instance, filename):
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    photo = models.ImageField(upload_to=user_directory_path, blank=True)
+    photo = models.ImageField(upload_to=user_directory_path, blank=True, verbose_name='фото')
 
     def __str__(self):
         return f'{self.username}'
@@ -23,12 +23,17 @@ class Task(models.Model):
     user = models.ForeignKey(get_user_model(),
                              on_delete=models.CASCADE,
                              related_name='user_tasks',
-                             blank=True)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    data_create = models.DateTimeField(auto_now_add=True)
-    color_code = models.CharField(max_length=255)
-    data_completed = models.DateTimeField()
+                             blank=True,
+                             verbose_name='пользователь')
+    title = models.CharField(max_length=255, verbose_name='заголовок')
+    description = models.TextField(verbose_name='описание')
+    data_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    color_code = models.CharField(max_length=255, verbose_name='цвет')
+    data_completed = models.DateTimeField(verbose_name='указанная дата')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'

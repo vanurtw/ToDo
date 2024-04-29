016a88d2b1a748e345b6fd6c9276f747fe17421f
vano <vanuartw@mail.ru>
2024-04-29 12:17:16 +0300

    users test error

--- ToDo/api/views.py
+++ ToDo/api/views.py
@@ -63,13 +63,14 @@ class TaskAPIView(GenericAPIView):
             data = request.user.user_tasks.filter(data_completed=date)
         else:
             data = request.user.user_tasks.all()
+        print(data)
         serializer = TaskSerializer(data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         data = request.data
         serializer = TaskSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
+        serializer.is_valid()
         serializer.save(user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 

640d857224125e0db0bfd2d0a2b4173a26080090
vano <vanuartw@mail.ru>
2024-05-02 11:22:29 +0300

    api admin-panel

--- ToDo/api/admin.py
+++ ToDo/api/admin.py
@@ -6,9 +6,11 @@ from .models import CustomUser, Task
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'email']
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ['id', 'title', 'user', 'data_completed', 'completed']
+    list_filter = ['user', 'data_create', 'data_completed']
+    search_fields = ['user', 'title', 'tag']
